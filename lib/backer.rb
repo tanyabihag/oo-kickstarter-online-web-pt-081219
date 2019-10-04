8f2017bb9e4ad15610a891b957c94a0c64508d0e
Tanya Bihag <tbihag@gmail.com>
2019-10-04 04:38:48 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,12 @@
+class Backer
+attr_reader :name, :backed_projects
+
+def initialize(name)
+  @name = name 
+  @backed_projects = []
+end 
+
+def backed_project(project)
+  @backed_project << project 
+end
+end
