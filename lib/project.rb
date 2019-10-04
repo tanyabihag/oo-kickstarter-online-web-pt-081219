b63b0b18eb3fcba89a1086e59782802acb187790
Tanya Bihag <tbihag@gmail.com>
2019-10-04 04:48:34 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,14 @@
+class Project
+
+  attr_reader :title, :backers
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(name)
+    @backers << name
+    name.backed_projects << self
+  end
+end 
